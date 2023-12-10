a8a50fc24f51b40360c939fc55036f13b119e997
Magdalena2013 <146621991+Magdalena2013@users.noreply.github.com>
2023-12-10 10:57:20 +0200

    main

---
+++ script.js
@@ -0,0 +1,125 @@
+//function funcName(message){
+//  console.log("function work proprely" + message)
+//   alert("It is a function" + message)
+//
+//}
+//
+//let message = 5434523452345243545234543544;
+//funcName(message, "Value 2")
+
+
+
+
+
+
+
+
+
+//
+//function myfunction(value){
+//    if(value < 25){
+//       alert("Less than need");
+//  } else if(value == 25){
+//      alert(YOU window);
+//    }
+//
+//}
+
+
+
+
+
+
+// let global = 3;
+
+
+// function calculator(a, b){
+//     let local = a + b + global;
+//     return local;
+// }
+
+// console.log(global)
+
+
+
+// let result = calculator(true, false);
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+// function calcPlus(a,b){
+// return a + b;
+// }
+// function calcMinus(a,b){
+//     return a + b;
+// }
+// function calcMulti(a,b){
+// return a + b;
+// }
+// function calcDiv(a,b){
+// return a + b;
+// }
+// let result = null;
+
+// function mainFunction(operation, a, b)
+//     if (operation == "+"){
+//         result = calcPlus(a, b);
+//         console.log(result);
+// }
+
+
+
+
+
+
+
+// let EmptyArray [];
+// let ArrayWithValue = ["Red", "Green", "Blue"];
+// // console.log(ArrayWithValue);
+// // console.log(ArrayWithValue[0]);
+// // console.log(ArrayWithValue[1]);
+// // console.log(ArrayWithValue[2]);
+// console.log(ArrayWithValue.length)
+// for (let i = 0; i < ArrayWithValue.length; i++){
+//     console.log(ArrayWithValue[i]);
+// }
+// for (let element of ArrayWithValue){
+//     console.log(element);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = [1]; 
+// array.push(2); //add smth at the end of the array
+// array.unshift(0); 
+// array.pop();
+// array.shift();
+// console.log(array);
+
+
+
+console.log(multidiminsional[2][2]);
+for (let i = 0, i < multidiminsional.length; i++){
+    for(let j = 0, j < multidiminsional.length; i++)
+        console.log(multidiminsional[i][j])
+}
